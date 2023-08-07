cabc4ae8e9b4cdb1b516fe835f58d5bcab47da39
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-07 16:06:07 +0100

    disemvowelTrolls

---
+++ disemvowelTrolls.js
@@ -0,0 +1,10 @@
+function disemvowel(str) {
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    
+    vowels.forEach(vowls => {
+        str = str.replaceAll(vowls, "");
+    })
+    return str
+  }
+
+  console.log(disemvowel("Hello World"))
