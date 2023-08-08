34abfa3f7b6cc01aea105bbc51e0c0f4619cde41
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-08 17:27:06 +0100

    CodeWars countingDuplicates Algorithm

---
+++ countingDuplicates.js
@@ -0,0 +1,22 @@
+function duplicateCount(text){
+    const textObj = {
+        
+    }
+    let textArr = text.toLowerCase().split("")
+    textArr.forEach(element => {
+      
+      if (element in textObj) {
+        textObj[element] += 1;
+      } else {
+        textObj[element] = 1;
+      }
+    });
+
+    // console.log(textArr);
+    // console.log(Object.entries(textObj))
+   const eachArr = Object.entries(textObj).filter(each => {
+        return each[1] > 1;
+    })
+    return eachArr.length
+}
+  console.log(duplicateCount("Hello"));
