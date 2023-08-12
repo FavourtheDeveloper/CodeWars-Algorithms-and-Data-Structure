18625a34d70eed0a516440a3009f6b18f1c4ab61
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-13 00:07:49 +0100

    CodeWars Algorithm

--- pigIt.js
+++ pigIt.js
@@ -2,19 +2,15 @@ function pigIt(str) {
   //Code here
   let ans = "";
   const wordd = str.split(" ");
-
   wordd.forEach((element) => {
     if (element == '!' || element == '.' || element == '\"' || element == '\'' || element == '?') {
       ans += element;
     } else {
       element += element[0];
-
-      const newEle = element.slice(1);
-
+      const newEle = element.slice(1); 
       ans += newEle + "ay ";
     }
   });
   return ans.trim();
 }
-
 console.log(pigIt("Hello world !"));
